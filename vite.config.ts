230f21207217eb37e820d13bcdcee57b75b4e66b
LAIA <louiabdalla1@gmail.com>
2026-05-17 18:46:14 +0300

    Fix: add base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/araak-logistics-website/', // هذا السطر المضاف ليخبر جيتهاب بالمسار الفرعي للمشروع
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
